96f5beaf5f1ea57b8fea3d02b4274606e8bb0615
Kashav Madan <kshvmdn@gmail.com>
2024-11-29 22:12:16 -0500

    Remove a directory

--- src/detect-client-side-script-source.js
+++
@@ -1,3 +0,0 @@
-// Differentiate a client-side script injected by a browser extension from a
-// script that was loaded as a result of the server side code.
-export function initClientSideScriptSourceDetection() {}
